ea8bc5547a7a3a1bb7e4834935a273601ae6b526
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-06-08 17:01:54 +0000

    Deploying to gh-pages from @ klaudiaszpigun/goit-react-hw-08-phonebook@b0832860136d421603aca9acc9f9b708efefa03a 🚀

---
+++ static/js/544.5b60a946.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,o,n){n.r(o);var r=n(184);o.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Home page"}),(0,r.jsx)("p",{children:"Welcome to your own online phonebook! Now you can save phone numbers."})]})}}}]);
+//# sourceMappingURL=544.5b60a946.chunk.js.map
